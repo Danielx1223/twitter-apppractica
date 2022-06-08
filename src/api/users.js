1df0217c92d7f3a6c1990ee0691027d5ef4e2521
Daniel Nieto <ndanielx@gmail.com>
2022-06-08 15:33:27 -0500

    add signIn

---
+++ src/api/users.js
@@ -0,0 +1,20 @@
+export async function signIn({ username, password }) {
+  // se encarga de recibir username y password para entrar
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/v1/users/signin`,
+    {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({ username, password }), // body data type must match "Content-Type" header
+    }
+  ); // para poner todos los end points de la api con el ID y sólo muestro lo de un tweet
+  const json = await response.json();
+  if (response.ok) {
+    return json;
+  } else {
+    return Promise.reject(json.message);
+  }
+}
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,23 +1,59 @@
 import { Button } from 'react-bootstrap';
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { signIn } from '../api/users';
 
 export default function SignIn() {
-  return (
-    <Form>
-      <Form.Group className='mb-3'>
-        <Form.Label>Username</Form.Label>
-        <Form.Control type='text' placeholder='Enter your username' />
-        <Form.Text className='text-muted'></Form.Text>
-      </Form.Group>
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  async function onSubmit(event) {
+    event.preventDefault();
+    const { username, password } = event.target.elements;
 
-      <Form.Group className='mb-3'>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Password' />
-      </Form.Group>
-      <Button variant='primary' type='submit'>
-        Sign In
-      </Button>
-    </Form>
+    try {
+      setError('');
+      const response = await signIn({
+        username: username.value,
+        password: password.value,
+      });
+      localStorage.setItem('token', response.meta.token);
+      localStorage.setItem('user', JSON.stringify(response.data)); // recibe objeto de javascript y lo convierte en cadena
+      navigate('/');
+    } catch (err) {
+      setError(err);
+    }
+  }
+  return (
+    <>
+      {error && ( // si esto es verdadero muestra alerta, sino no
+        <div class='alert alert-danger' role='alert'>
+          {error}
+        </div>
+      )}
+      <Form onSubmit={onSubmit}>
+        <Form.Group className='mb-3'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type='text'
+            name='username'
+            placeholder='Enter your username'
+          />
+          <Form.Text className='text-muted'></Form.Text>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            name='password'
+            placeholder='Password'
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Sign In
+        </Button>
+      </Form>
+    </>
   );
 }
